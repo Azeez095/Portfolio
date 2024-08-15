53c108c2efab2cdd3264ab5a13bbd7b9746475cb
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-08-15 16:15:21 +0100

    Initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,159 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import img1 from "../assets/img/img1.png";
+import img2 from "../assets/img/img2.png";
+import img3 from "../assets/img/img3.png";
+import img4 from "../assets/img/img4.png";
+import img5 from "../assets/img/img5.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    }
+    
+  ];
+  const project2 = [
+    {
+      title: "Ecommerce",
+      description: "Frontend-Development",
+      imgUrl: img1,
+      link:"https://akolade-store.netlify.app/"
+    },
+    {
+      title: "Business Startup",
+      description: "Frontend-Development",
+      imgUrl: img4,
+      link: "https://azeez-basketball.netlify.app/"
+    },
+    {
+      title: "Travel Journal",
+      description: "Frontend-Development",
+      imgUrl: img3,
+      link: "https://azeez-traveljournal.netlify.app/"
+    },
+  ]
+  const project3 = [
+    {
+      title: "Travel",
+      description: "Frontend-Development",
+      imgUrl: img2,
+      link:"https://vanlife-akolade.netlify.app/"
+    },
+    {
+      title: "Business Startup",
+      description: "Frontend-Development",
+      imgUrl: img4,
+      link: "https://basketball-scores-record.netlify.app/"
+    },
+    {
+      title: "Ecommerce",
+      description: "Frontend-Development",
+      imgUrl: img1,
+      link:"https://akolade-store.netlify.app/"
+    }
+  ]
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          project2.map((projects, index) => {
+                          return (
+                          <ProjectCard
+                          key={index}
+                          {...projects}
+                          />
+                        
+                         
+                      )
+                    })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                      
+                        {
+                       
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
+                              
+                              
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <Row>
+                      
+                      {
+                     
+                            project3.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                  />
+                              )
+                            })
+                                   
+                      }
+                    </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
